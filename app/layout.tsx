e5c6e541aef5bbbdcd43a9e417cc736cdd96a5b9
diasirga07-max <diasirga07@gmail.com>
2026-09-01 00:12:13 +0500

    style: load GRANTS BOOK styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Link from "next/link";
 import "./globals.css";
+import "./grants-book.css";
 
 export const metadata: Metadata = {
   title: "Marketplace Manager",
