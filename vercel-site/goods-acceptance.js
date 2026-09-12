c42f02035a11ef9de7b4636ae051da6818afd85e
diasirga07-max <diasirga07@gmail.com>
2026-09-13 01:04:49 +0500

    Switch New Orders to live Kaspi V2 module

--- vercel-site/goods-acceptance.js
+++ vercel-site/goods-acceptance.js
@@ -60,8 +60,7 @@ function loadScript(id,url,flag,label){
   }).catch(e=>{console.error(label+' load failed',e);setTimeout(()=>loadScript(id,url,flag,label),3000)});
 }
 function loadNewOrdersModule(){
-  loadScript('gbNewOrdersRuntime','https://raw.githubusercontent.com/diasirga07-max/marketplace-manager/main/public/gb-new-orders.js?v=20260912-2','GB_NEW_ORDERS_LOADED','New orders module');
-  loadScript('gbNewOrdersPhotoFixRuntime','https://raw.githubusercontent.com/diasirga07-max/marketplace-manager/main/public/gb-new-orders-photo-fix.js?v=20260912-2','GB_NEW_ORDERS_PHOTO_FIX_LOADED','New orders photo fix');
+  loadScript('gbNewOrdersRuntime','https://raw.githubusercontent.com/diasirga07-max/marketplace-manager/main/public/gb-new-orders-v2.js?v=20260913-1','GB_NEW_ORDERS_LOADED','New orders LIVE module');
 }
 
 function guard(){disableAcceptance();ensureStopButton();loadNewOrdersModule()}
